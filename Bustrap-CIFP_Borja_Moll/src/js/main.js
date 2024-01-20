bfca1352d9943f55f76ed61df73aa18c48299f38
noecrespi <ncrespipomar@cifpfbmoll.eu>
2024-01-20 12:09:07 +0100

    add: implements bootstrap and webpack in the project

---
+++ Bustrap-CIFP_Borja_Moll/src/js/main.js
@@ -0,0 +1,7 @@
+import "../bootstrap-5.3.2/scss/bootstrap.scss";
+
+// Import our custom CSS
+// import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
