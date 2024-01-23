6e4f52e99f1c73f903c14cf2c1ad392f6897f241
Noelia <ncrespipomar@cifpfbmoll.eu>
2024-01-23 17:58:28 +0100

    Delete Bustrap-CIFP_Borja_Moll directory

--- Bustrap-CIFP_Borja_Moll/src/js/main.js
+++
@@ -1,7 +0,0 @@
-import "../bootstrap-5.3.2/scss/bootstrap.scss";
-
-// Import our custom CSS
-// import '../scss/styles.scss'
-
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
